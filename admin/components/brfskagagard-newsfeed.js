26bbbd6e1276cda81016560701076bdb2d228543
Mattias <github-ejtu5oeiughpiun4p@email.flowertwig.org>
2016-11-29 21:49:36 +0100

    some newsfeed work

--- admin/components/brfskagagard-newsfeed.js
+++ admin/components/brfskagagard-newsfeed.js
@@ -9,6 +9,43 @@
         return this.init(element);
     }
     NewsFeed.prototype = {
+        createTinyMCEInstance: function (element) {
+            var self = this;
+            if (!self._loaded) {
+                self._loaded = true;
+                staticWeb.includeScript("https://cdn.tinymce.com/4/tinymce.min.js");
+            }
+            staticWeb.ensureLoaded('tinymce', window, function () {
+                if (!element.id) {
+                    element.id = 'newsfeed-' + new Date().getTime();
+                }
+
+                var toolbar = '';
+                switch (element.tagName.toLowerCase()) {
+                    case 'h2':
+                    case 'time':
+                        toolbar = "save | undo redo";
+                        break;
+                    case 'p':
+                        toolbar = "save | bold italic | bullist numlist outdent indent | link image | undo redo";
+                        break;
+                }
+
+                // TODO: see if element in question have a 'data-staticweb-component-swtext-data' attribute and use that for the toolbar options
+                tinymce.init({
+                    selector: '#' + element.id,
+                    inline: true,
+                    menubar: false,
+                    browser_spellcheck: true,
+                    plugins: "save",
+                    toolbar: toolbar,
+                    save_onsavecallback: self.save
+                });
+            });
+        },
+        save: function (editor) {
+
+        },
         createInterface: function () {
             var self = this;
 
@@ -17,8 +54,46 @@
                 // get news item template
                 self._itemTemplate = self._template.querySelector('newsfeed-item').children[0];
 
-                staticWeb.insertTemplate(self._itemTemplate, self._element);
+                self._addNewItemTemplate = self._template.querySelector('add-newsfeed').children[0];
+
+                if (self._element.children.length === 0) {
+                    staticWeb.insertTemplate(self._addNewItemTemplate, self._element);
+                }
+
+                var items = self._element.querySelectorAll('.newsfeed-add');
+                for (var i = 0; i < items.length; i++) {
+                    var button = items[i];
+                    button.addEventListener('click', function (event) {
+                        self.addButtonClickAction(event);
+                    });
+                }
             });
+        },
+        addButton: function (element) {
+            var self = this;
+            var btn = self._addNewItemTemplate.cloneNode(true);
+            element.insertAdjacentElement('afterend', btn);
+
+            btn.addEventListener('click', function (event) {
+                self.addButtonClickAction(event);
+            });
+        },
+        addButtonClickAction: function (event) {
+            var self = this;
+            event.preventDefault();
+
+            var newsItem = self._itemTemplate.cloneNode(true);
+            event.target.insertAdjacentElement('afterend', newsItem);
+            self.createTinyMCEInstance(newsItem.querySelector('h2'));
+
+            var timeElement = newsItem.querySelector('time');
+            var date = new Date();
+            timeElement.innerText = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
+            timeElement.setAttribute('datetime', timeElement.innerText);
+
+            self.createTinyMCEInstance(newsItem.querySelector('time'));
+            self.createTinyMCEInstance(newsItem.querySelector('p'));
+            self.addButton(newsItem);
         },
         init: function (element) {
             var self = this;
